565fc3d167b8e0266ca6140fa48ca6c9e9f27737
maramboussalem <marammaramboussalem@gmail.com>
2025-11-14 11:41:14 +0100

    Création de Contrôleur

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,              // Supprime les champs NON autorisés
-      forbidNonWhitelisted: true,   // Renvoie une erreur si un champ est interdit
-      transform: true,              // Convertit automatiquement les types (ex: id string → number)
-    }),
-  );
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,90 +1,64 @@
-import { Controller ,Get,Post,Put,Delete,Param,Body,Query,Headers } from '@nestjs/common';
+import { Controller ,Get,Query,Post,Put,Delete,Param,Body,Headers } from '@nestjs/common';
 
+
+let users = [
+   { id: 1, username: 'Mohamed', email: 'mohamed@esprit.tn', status: 'active' },
+   { id: 2, username: 'Sarra', email: 'sarra@esprit.tn', status: 'inactive' },
+   { id: 3, username: 'Ali', email: 'ali@esprit.tn', status: 'inactive' },
+   { id: 4, username: 'Eya', email: 'eya@esprit.tn', status: 'active' },
+ ]; 
+ 
 @Controller('users')
 export class UsersController {
- users = [
-    { id: 1, username: 'Mohamed', email: 'mohamed@esprit.tn', status: 'active' },
-    { id: 2, username: 'Sarra', email: 'sarra@esprit.tn', status: 'inactive' },
-    { id: 3, username: 'Ali', email: 'ali@esprit.tn', status: 'inactive' },
-    { id: 4, username: 'Eya', email: 'eya@esprit.tn', status: 'active' },
-  ]; 
-  
-  // ---------------------------------------------------
-  // 1. Récupérer tous les utilisateurs + filtrage query
-  // URL : GET /users
-  // URL filtrée : GET /users?status=active
-  // ---------------------------------------------------
+// ---------------------------------------------------
+// 1. Récupérer tous les utilisateurs + filtrage query
+// URL : GET /users
+// URL filtrée : GET /users?username=Mouhamed
+// ---------------------------------------------------
   @Get()
-  getAllUsers(@Query('status') status?: string) {
-    if (status) {
-      return this.users.filter((user) => user.status === status);
+  findAll(@Query('username') username: string) {
+    if (username) {
+      return users.filter(user => user.username === username);
     }
-    return this.users;
+    return users;
   }
 
-  // ---------------------------------------------------
-  // 2. Récupérer un utilisateur par ID
-  // URL : GET /users/3
-  // ---------------------------------------------------
   @Get(':id')
-  getUserById(@Param('id') id: number) {
-    return this.users.find((user) => user.id == id);
+  findOne(@Param('id') id: number) {
+    return users.find((user) => user.id === Number(id));
   }
 
-  // ---------------------------------------------------
-  // 3. Ajouter un nouvel utilisateur
-  // URL : POST /users
-  // Body : { "username": "...", "email": "...", "status": "..." }
-  // Header : Authorization: token-secret
-  // ---------------------------------------------------
   @Post()
-  createUser(
-    @Body() userData: any,
-    @Headers('authorization') token: string,
-  ) {
-    console.log('TOKEN envoyé par le client : ', token);
-
-    const newUser = {
-      id: this.users.length + 1,
-      ...userData,
-    };
-
-    this.users.push(newUser);
-    return newUser;
-  }
-    // ---------------------------------------------------
-  @Put(':id')
-  updateUser(@Param('id') id: number, @Body() data: any) {
-    const user = this.users.find((u) => u.id == id);
-
-    if (!user) return { message: 'Utilisateur introuvable' };
-
-    Object.assign(user, data); // Mise à jour propre
-
-    return user;
+  create(@Headers('authorization') auth: string, @Body() data){
+   console.log('Token envoyé par le client : ', auth);
+   const newUser ={
+     id: Date.now(),
+     ...data,
+   }
+   users.push(newUser);
+   return users;
   }
 
-  // ---------------------------------------------------
-  // 5. Supprimer un utilisateur
-  // URL : DELETE /users/3
-  // ---------------------------------------------------
+ @Put(':id')
+ update(@Param('id') id: number, @Body() data){
+  const foundUser = users.find((u) => u.id === Number(id));
+  if(foundUser){
+    foundUser.username = data.username;
+    foundUser.email = data.email;
+    foundUser.status = data.status;
+    return foundUser;
+  }
+  return "notfound";
+ }
+
   @Delete(':id')
-  deleteUser(@Param('id') id: number) {
-    const before = this.users.length;
-    this.users = this.users.filter((u) => u.id != id);
-    const after = this.users.length;
-
-    return before !== after
-      ? { message: 'Utilisateur supprimé' }
-      : { message: 'Utilisateur introuvable' };
+  remove(@Param('id') id: number){
+    const index = users.findIndex((u) => u.id === Number(id));
+    if(index !== -1){
+      users.splice(index, 1);
+      return { message: 'User deleted successfully' };
+    }
+    return { message: 'User not found' };
   }
 
-  // ---------------------------------------------------
-  // 6. Route spéciale : afficher les users actifs
-  // URL : GET /users/active/active
-  // ---------------------------------------------------
-  @Get('active/:status')
-  getUsersByStatus(@Param('status') status: string) {
-    return this.users.filter((u) => u.status === status);
-  }
 }
